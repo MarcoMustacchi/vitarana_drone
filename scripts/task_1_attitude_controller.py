c24f51de86263bceefd543dce13456d7fc80fd99
Marco <marcomustacchi@gmail.com>
2022-11-12 17:16:02 +0100

    Adding Sensors Subscriber and Actuators Publisher

--- scripts/task_1_attitude_controller.py
+++ scripts/task_1_attitude_controller.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
 
-class ClassName (object):
+import rospy
+import tf
+from vitarana_drone.msg import *
+from sensor_msgs.msg import Imu
+
+class Edrone ():
 
 	def __init__(self,):
-		pass
+		self.actual_quaternion_orientation = [0.0, 0.0, 0.0, 0.0]
+		self.actual_euler_orientation = [0.0, 0.0, 0.0]
+		self.desired_euler_orientation = [0.0, 0.0, 0.0]
 
+	rospy.Subscriber('/edrone/imu', Imu, self.imu_callback)
+
+	self.rpyt_pub = rospy.Publisher('/drone_command', edrone_cmd, queue_size=1)
+
+	def imu_callback(self, msg):
+		self.actual_quaternion_orientation[0] = msg.orientation.x
+		self.actual_quaternion_orientation[1] = msg.orientation.y
+		self.actual_quaternion_orientation[2] = msg.orientation.z
+		self.actual_quaternion_orientation[3] = msg.orientation.w
+		(actual_euler_orientation[0], actual_euler_orientation[1], actual_euler_orientation[2]) = tf.transformation.euler_from_quaternion(self.actual_quaternion_orientation[0], self.actual_quaternion_orientation[1], self.actual_quaternion_orientation[2], self.actual_quaternion_orientation[3])
 
 
 if __name__ == '__main__':
-	sys.exit(main())
+
+	drone = Edrone()
 
--- scripts/task_1_position_controller.py
+++ scripts/task_1_position_controller.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 
-class ClassName (object):
+import rospy
+from vitarana_drone.msg import *
+from sensor_msgs.msg import NavSatFix
+
+
+class Edrone ():
 
 	def __init__(self,):
-		pass
+		self.actual_location = [0.0, 0.0, 0.0]
+		self.desired_location = [19.0000451704, 72.0, 3.0]
 
+	rospy.Subscriber('/edrone/gps', NavSatFix, self.gps_callback)
+
+	self.rpyt_pub = rospy.Publisher('/drone_command', edrone_cmd, queue_size=1)
+
+	def gps_callback(self):
+		self.actual_location[0] = msg.latitude
+		self.actual_location[1] = msg.longitude
+		self.actual_location[2] = msg.altitude
 
 
 if __name__ == '__main__':
-	sys.exit(main())
 
-
-
-
+	drone = Edrone()
 
