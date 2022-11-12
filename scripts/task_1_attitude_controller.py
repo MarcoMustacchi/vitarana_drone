373e57725d85a25557562f4dfd9386d52770a4c5
Marco <marcomustacchi@gmail.com>
2022-11-12 17:13:10 +0100

    Adding Task1 position and attitude controller

---
+++ scripts/task_1_attitude_controller.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+class ClassName (object):
+
+	def __init__(self,):
+		pass
+
+
+
+if __name__ == '__main__':
+	sys.exit(main())
+
---
+++ scripts/task_1_position_controller.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+class ClassName (object):
+
+	def __init__(self,):
+		pass
+
+
+
+if __name__ == '__main__':
+	sys.exit(main())
+
+
+
+
+
